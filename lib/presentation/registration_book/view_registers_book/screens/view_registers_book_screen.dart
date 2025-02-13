5e91b105ab4afc8a98373f75c72bf3d9b1444768
4dominic888 <4dominin888@gmail.com>
2025-02-13 12:47:57 -0500

    group list pero mas manejable

--- lib/presentation/registration_book/view_registers_book/screens/view_registers_book_screen.dart
+++ lib/presentation/registration_book/view_registers_book/screens/view_registers_book_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:grouped_list/grouped_list.dart';
 import 'package:intl/intl.dart';
+import 'package:group_list_view/group_list_view.dart';
+import 'package:collection/collection.dart';
 
 class ViewRegistersBookScreen extends StatefulWidget {
   const ViewRegistersBookScreen({super.key});
@@ -13,7 +13,7 @@ class ViewRegistersBookScreen extends StatefulWidget {
 class _ViewRegistersBookScreenState extends State<ViewRegistersBookScreen> {
 
   //TODO: Cambiar por origen de datos real
-  final List<Map<String, dynamic>> registersBook = List.filled(3, [
+  final List<Map<String, dynamic>> registersBook = List.filled(4, [
     {"action": "Lorem Ipsum Dolor sit amet Asit aabbss", "date": DateTime(2025, 1, 15), "emisors": ["Pepe", "Pablo"]},
     {"action": "Amet Silli dolor Awe Dea Cajl", "date": DateTime(2025, 1, 15), "emisors": ["María José"]},
     {"action": "Sit Ipsum Lorem Amet Siti Coja", "date": DateTime(2025, 1, 13), "emisors": ["Mario", "Fernanda", "Julian", "Pepe", "Jose"]},
@@ -35,12 +35,10 @@ class _ViewRegistersBookScreenState extends State<ViewRegistersBookScreen> {
             confirmDismiss: (direction) async {
               if(direction == DismissDirection.startToEnd) {
                 //* Action when edit
-                return false;
               }
               if(direction == DismissDirection.endToStart) {
                 //* Action when delete
                 //TODO: agregar un modal para confirmar la eliminacion.
-                return true;
               }
               return false;
             },
@@ -82,28 +80,40 @@ class _ViewRegistersBookScreenState extends State<ViewRegistersBookScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    final groupedList = groupBy(registersBook, (e) => DateTime((e["date"] as DateTime).year, (e["date"] as DateTime).month, (e["date"] as DateTime).day));
+
     return Scaffold(
       body: RefreshIndicator(
         onRefresh: () async { },
-        child: ScrollConfiguration(
-          behavior: ScrollConfiguration.of(context).copyWith(overscroll: true, dragDevices: {PointerDeviceKind.mouse}),
-
-          //TODO: Cambiar Map por el tipo de dato real.
-          child: GroupedListView< Map<String, dynamic> , DateTime>(
-            elements: registersBook,
-            groupBy: (e) {
-              final date = e["date"] as DateTime;
-              return DateTime(date.year, date.month, date.day);
-            },
-            groupSeparatorBuilder: (value) => ListTile(title: Text(
-              DateFormat('EEEE, d MMMM y').format(value),
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
-            )),
-            itemBuilder: (c, element) => registersBook.isNotEmpty ? _registersBookList(c, element) : 
-              GridView.count(crossAxisCount: 1, children: [const Center(child: Text("No hay registros..."))]),
-            order: GroupedListOrder.DESC,
-            itemComparator: (el1, el2) => (el1["date"] as DateTime).compareTo(el2["date"] as DateTime),
-          ),
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          child: GroupListView(
+            sectionsCount: groupedList.keys.length,
+            countOfItemInSection: (section) => groupedList.values.elementAt(section).length,
+            itemBuilder: (_, index) => _registersBookList(_, groupedList.values.elementAt(index.section).elementAt(index.index)),
+            groupHeaderBuilder: (_, section) => Text(DateFormat('EEEE, d MMMM y').format(groupedList.keys.elementAt(section))),
+            separatorBuilder: (_, index) => const SizedBox(height: 10),
+            sectionSeparatorBuilder: (_, section) => const SizedBox(height: 10),
+          )
+          // child: GroupedListView< Map<String, dynamic> , DateTime>(
+          //   useStickyGroupSeparators: true,
+          //   addSemanticIndexes: true,
+          //   elements: registersBook,
+          //   sort: true,
+          //   groupBy: (e) {
+          //     final date = e["date"] as DateTime;
+          //     return DateTime(date.year, date.month, date.day);
+          //   },
+          //   groupSeparatorBuilder: (value) => ListTile(title: Text(
+          //     DateFormat('EEEE, d MMMM y').format(value),
+          //     style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+          //   )),
+          //   itemBuilder: (c, element) => registersBook.isNotEmpty ? _registersBookList(c, element) : 
+          //     GridView.count(crossAxisCount: 1, children: [const Center(child: Text("No hay registros..."))]),
+          //   order: GroupedListOrder.DESC,
+          //   itemComparator: (el1, el2) => (el1["date"] as DateTime).compareTo(el2["date"] as DateTime),
+          // ),
         )
       ),
     );
