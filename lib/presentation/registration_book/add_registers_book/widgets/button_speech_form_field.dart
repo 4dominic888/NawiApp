9390be0bba084bac209ce021aa8ef4503c225202
4dominic888 <4dominin888@gmail.com>
2025-02-12 20:31:13 -0500

    widget de texto a voz en progreso

---
+++ lib/presentation/registration_book/add_registers_book/widgets/button_speech_form_field.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ButtonSpeechFormField extends StatefulWidget {
+  const ButtonSpeechFormField({super.key});
+
+  @override
+  State<ButtonSpeechFormField> createState() => _ButtonSpeechFormFieldState();
+}
+
+class _ButtonSpeechFormFieldState extends State<ButtonSpeechFormField> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
