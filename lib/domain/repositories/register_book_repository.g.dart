84788a63d078c9ef14fc7fd401e6a7e6f5b64941
4dominic888 <4dominin888@gmail.com>
2025-02-22 22:05:36 -0500

    repositorios (DAOs)

---
+++ lib/domain/repositories/register_book_repository.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'register_book_repository.dart';
+
+// ignore_for_file: type=lint
+mixin _$RegisterBookRepositoryMixin on DatabaseAccessor<NawiDatabase> {
+  $RegisterBookTableTable get registerBookTable =>
+      attachedDatabase.registerBookTable;
+  $StudentTableTable get studentTable => attachedDatabase.studentTable;
+  $StudentRegisterBookTableTable get studentRegisterBookTable =>
+      attachedDatabase.studentRegisterBookTable;
+  $RegisterBookViewDAOVersionView get registerBookViewDAOVersion =>
+      attachedDatabase.registerBookViewDAOVersion;
+}
