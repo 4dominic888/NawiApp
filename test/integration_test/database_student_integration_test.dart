3e650e3d14e7d785587751b3f9b8e9a503393b57
4dominic888 <4dominin888@gmail.com>
2025-03-25 15:01:05 -0500

    test de estudiantes

---
+++ test/integration_test/database_student_integration_test.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:nawiapp/domain/classes/result.dart';
+import 'package:nawiapp/domain/classes/student_filter.dart';
+import 'package:nawiapp/domain/models/models_views/student_view.dart';
+import 'package:nawiapp/domain/models/student.dart';
+import 'package:nawiapp/domain/services/student_service_base.dart';
+
+import '../nawi_test_utils.dart';
+
+void main(){
+  setUp(NawiTestUtils.setupTestLocator);
+  tearDown(NawiTestUtils.onTearDownSetupLocator);
+
+  test('Registro de un estudiante', () async {
+    final service = GetIt.I<StudentServiceBase>();
+
+    final student = Student(name: "Pepe Gonzales", age: StudentAge.fourYears);
+    final diffStudent = Student(name: "Pepe Gonzales2", age: StudentAge.fourYears);
+
+    final result = await service.addOne(student);
+    final studentFromDatabaseDAO = (await service.getAll(StudentFilter(nameLike: "pepe"))).getValue!.first;
+
+    //? Se compara por nombres, ya que [student] no tiene una ID asignada, pero para verificar que devuelva un estudiante coherente
+    expect(result.getValue!.name, student.name); //* El retorno de la función de AddOne() es correcto
+    debugPrint("Expect 1 of 3 for AddOne() passed!");
+    expect(result.getValue!.name, isNot(diffStudent.name)); //* Corrobora con un estudiante erróneo
+    debugPrint("Expect 2 of 3 for AddOne() passed!");
+    expect(studentFromDatabaseDAO, result.getValue); //* Se verifica que exista en la base de datos
+    debugPrint("Expect 3 of 3 for AddOne() passed!");
+  });
+
+  test('Eliminacion de un estudiante', () async {
+    final service = GetIt.I<StudentServiceBase>();
+
+    final addedResult = await service.addOne(Student(name: "Pepe Gonzales", age: StudentAge.fourYears));
+    final result = await service.deleteOne(addedResult.getValue!.id);
+
+    expect(result.getValue!.id, addedResult.getValue!.id); //* Se espera que el valor devuelto sea del estudiante eliminado
+    debugPrint("Expect 1 of 2 for DeleteOne() passed!");
+    
+    final getDeletedResult = await service.getOne(result.getValue!.id);
+    expect(getDeletedResult.runtimeType, NawiError<Student>); //* Se espera que no esté en la BD
+    debugPrint("Expect 2 of 2 for DeleteOne() passed!");
+  });
+
+  test("Actualizacion de un estudiante", () async {
+    final service = GetIt.I<StudentServiceBase>();
+
+    final addedResult = await service.addOne(Student(name: "Pepe Gonzales", age: StudentAge.fourYears));
+    final result = await service.updateOne(addedResult.getValue!.copyWith(name: "Amaterasu Remake", age: StudentAge.fiveYears));
+
+    final getResult = await service.getOne(addedResult.getValue!.id);
+
+    expect(result.getValue!, true); //* Segun el result, debería de haberse ejecutado correctamente
+    debugPrint("Expect 1 of 3 for UpdateOne() passed!");
+    expect(getResult.getValue!.name, "Amaterasu Remake"); //* En la consulta a la BD, se debe reflejar los cambios
+    debugPrint("Expect 2 of 3 for UpdateOne() passed!");
+    expect(getResult.getValue!.id, addedResult.getValue!.id); //* Las IDs deben coincidir
+    debugPrint("Expect 2 of 3 for UpdateOne() passed!");
+  });
+
+  test('Obtener un estudiante', () async {
+    final service = GetIt.I<StudentServiceBase>();
+
+    final badResult = await service.getOne('0');
+    final goodResult = await service.getOne('980bbf70-48de-4946-8d8f-de06a39d7611');
+
+    expect(badResult.runtimeType, NawiError<Student>); //* Se espera que no se encuentre el valor ante un ID no existente
+    debugPrint("Expect 1 of 2 for getOne() passed!");
+    expect(goodResult.getValue!.name, "Leonardo Da Vinci"); //* Se espera que el estudiante obtenido exista en la base de datos
+    debugPrint("Expect 2 of 2 for getOne() passed!");
+  });
+
+  test('Archivado y Desarchivado de un estudiante', () async {
+    final service = GetIt.I<StudentServiceBase>();
+    
+    final studentArchived = Student(name: "Juan gonzales paredes", age: StudentAge.fourYears);
+    final student = Student(name: "Juan Archive", age: StudentAge.fourYears);
+    
+    final archivedAddedResult = await service.addOne(studentArchived);
+    await service.addOne(student);
+
+    var result = await service.getAll(StudentFilter());
+    int length = result.getValue!.length;
+    
+    await service.archiveOne(archivedAddedResult.getValue!.id);
+
+    result = await service.getAll(StudentFilter());
+    expect(result.getValue!.length, length - 1); //* Deberia quitarse el estudiante archivado de la lista
+    debugPrint("Expect 1 of 1 for archiveOne() passed!");
+
+    await service.unarchiveOne(archivedAddedResult.getValue!.id);
+    result = await service.getAll(StudentFilter());
+    expect(result.getValue!.length, length); //* Deberia volver a ingresarse el estudiante archivado
+    debugPrint("Expect 1 of 1 for unarchiveOne() passed!");
+  });
+
+  group('Filtro de estudiantes', () {
+    test('Ordenamiento de estudiantes', () async {
+      final service = GetIt.I<StudentServiceBase>();
+
+      expect(( //* Prueba por order por nombre ascendentemente
+        await service.getAll(
+          StudentFilter(orderBy: StudentViewOrderByType.nameAsc, pageSize: 1, currentPage: 0)
+        )
+      ).getValue!.first.name, NawiTestUtils.studentHighestName.name);
+      debugPrint("Expect 1 of 4 for getAll() about OrberBy passed!");
+
+      expect(( //* Prueba por nombre descendentemente
+        await service.getAll(
+          StudentFilter(orderBy: StudentViewOrderByType.nameDesc, pageSize: 1, currentPage: 0)
+        )
+      ).getValue!.first.name, NawiTestUtils.studentLowestName.name);
+      debugPrint("Expect 2 of 4 for getAll() about OrberBy passed!");
+
+      expect(( //* Prueba por estudiante registrado recientemente
+        await service.getAll(
+          StudentFilter(orderBy: StudentViewOrderByType.timestampRecently, pageSize: 1, currentPage: 0)
+        )
+      ).getValue!.first.name, NawiTestUtils.studentRecently.name);
+      debugPrint("Expect 3 of 4 for getAll() about OrberBy passed!");
+
+      expect(( //* Prueba por estudiante registrado viejo
+        await service.getAll(
+          StudentFilter(orderBy: StudentViewOrderByType.timestampOldy, pageSize: 1, currentPage: 0)
+        )
+      ).getValue!.first.name, NawiTestUtils.studentOldy.name);
+      debugPrint("Expect 4 of 4 for getAll() about OrberBy passed!");
+    });
+
+    test('Paginado de estudiantes', () async {
+      final service = GetIt.I<StudentServiceBase>();
+
+      expect(( //* Prueba que solo se regrese 3 elementos
+        await service.getAllPaginated(pageSize: 3, currentPage: 1, params: StudentFilter())
+      ).getValue!.data.length, 3);
+      debugPrint("Expect 1 of 2 for getAll() about Pagination passed!");
+
+      expect(( //* Prueba que solo se regrese 2 elementos
+        await service.getAllPaginated(pageSize: 4, currentPage: 2, params: StudentFilter())
+      ).getValue!.data.length, 2);
+      debugPrint("Expect 1 of 2 for getAll() about Pagination passed!");
+    });
+
+    test('Busqueda por nombre de estudiante', () async {
+      final service = GetIt.I<StudentServiceBase>();
+
+      expect((
+        await service.getAll(StudentFilter(nameLike: "l"))
+      ).getValue!.any((e) => e.name == 'Jose Olaya'), true);
+      debugPrint("Expect 1 of 3 for getAll() about SearchBy name passed!");
+
+      expect((
+        await service.getAll(StudentFilter(nameLike: "ñ"))
+      ).getValue!.isEmpty, true);
+      debugPrint("Expect 2 of 3 for getAll() about SearchBy name passed!");
+
+      expect(( //* Como si no se aplicara el filtro
+        await service.getAll(StudentFilter(nameLike: "  "))
+      ).getValue!.length, 6);
+      debugPrint("Expect 3 of 3 for getAll() about SearchBy name passed!");
+    });
+
+    test('Busqueda por edad del estudiante', () async {
+      final service = GetIt.I<StudentServiceBase>();
+
+      expect((
+        await service.getAll(StudentFilter(ageEnumIndex1: StudentAge.fiveYears))
+      ).getValue!.any((element) => element.age == StudentAge.fiveYears), true);
+      debugPrint("Expect 1 of 3 for getAll() about SearchBy age passed!");
+
+      expect((
+        await service.getAll(StudentFilter(ageEnumIndex1: StudentAge.fourYears, ageEnumIndex2: StudentAge.threeYears))
+      ).getValue!.length, 5);
+      debugPrint("Expect 2 of 3 for getAll() about SearchBy age passed!");
+      
+      expect((
+        await service.getAll(StudentFilter(ageEnumIndex2: StudentAge.fourYears))
+      ).getValue!.length, 2);
+      debugPrint("Expect 3 of 3 for getAll() about SearchBy age passed!");
+    });
+
+    test('Filtro de estudiantes archivados', () async {
+      final service = GetIt.I<StudentServiceBase>();
+
+      expect(( //* Solo hay 2 estudiantes archivados en la lista estática
+        await service.getAll(StudentFilter(showHidden: true))
+      ).getValue!.length, 2);
+      debugPrint("Expect 1 of 1 for getAll() about archived students passed!");
+    });
+
+  });
+}
---
+++ test/nawi_test_utils.dart
@@ -0,0 +1,100 @@
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:get_it/get_it.dart';
+import 'package:nawiapp/data/database_connection.dart';
+import 'package:nawiapp/domain/classes/register_book_filter.dart';
+import 'package:nawiapp/domain/classes/student_filter.dart';
+import 'package:nawiapp/domain/models/register_book.dart';
+import 'package:nawiapp/domain/models/student.dart';
+import 'package:nawiapp/domain/repositories/register_book_repository.dart';
+import 'package:nawiapp/domain/repositories/student_register_book_repository.dart';
+import 'package:nawiapp/domain/repositories/student_repository.dart';
+import 'package:nawiapp/domain/services/register_book_service_base.dart';
+import 'package:nawiapp/domain/services/student_service_base.dart';
+import 'package:nawiapp/presentation/registration_book/implementations/register_book_service_implement.dart';
+import 'package:nawiapp/presentation/students/implementations/student_service_implement.dart';
+
+class NawiTestUtils {
+  static final GetIt _locator = GetIt.instance;
+
+  static Future<void> setupTestLocator({bool withRegisterBook = false}) async {
+    await _locator.reset();
+
+    _locator.registerLazySingleton<NawiDatabase>(() => NawiDatabase(DatabaseConnection(
+      NativeDatabase.memory(),
+      closeStreamsSynchronously: true
+    )));
+
+    _locator.registerLazySingleton<StudentRepository>(() => StudentRepository(_locator<NawiDatabase>()));
+    _locator.registerLazySingleton<RegisterBookRepository>(() => RegisterBookRepository(_locator<NawiDatabase>()));
+    _locator.registerLazySingleton<StudentRegisterBookRepository>(() => StudentRegisterBookRepository(_locator<NawiDatabase>()));
+
+    _locator.registerLazySingleton<StudentServiceBase>(() => 
+      StudentServiceImplement(_locator<StudentRepository>(), _locator<StudentRegisterBookRepository>())
+    );
+    _locator.registerLazySingleton<RegisterBookServiceBase>(() => 
+      RegisterBookServiceImplement(_locator<RegisterBookRepository>(), _locator<StudentRegisterBookRepository>())
+    );
+
+    await NawiTestUtils.addingTestData(withRegisterBook);
+  }
+
+  static Future<void> onTearDownSetupLocator() async {
+    await _locator<NawiDatabase>().close();
+    await _locator.reset();
+  }
+
+  static final listOfStudents = <Student>[
+    Student(id: '1d03982a-7a0a-40f2-adb4-1e90c2550485', name: "Jose Olaya", age: StudentAge.threeYears).copyWith(timestamp: DateTime(2025, 1, 8)),
+    Student(id: '9d95e4ee-8706-485d-b2a9-614583a05296', name: "Pablo Rojas", age: StudentAge.fourYears).copyWith(timestamp: DateTime(2025, 1, 9)),
+    Student(id: '580e4114-98c8-4b80-92ea-b44c140f26e3', name: "Mendo Cabrera", age: StudentAge.fiveYears).copyWith(timestamp: DateTime(2025, 2, 1)),
+    Student(id: '194c4084-0fdf-49f2-86d7-766b7607ce0b', name: "Bruno Sorias Alias El Cumbia Ninja", age: StudentAge.threeYears).copyWith(timestamp: DateTime(2025, 5, 1)),
+    Student(id: '8722e6e9-6178-4296-948a-7fb3db196d44', name: "Gerardo Días", age: StudentAge.fourYears).copyWith(timestamp: DateTime(2025, 3, 20)),
+    Student(id: '980bbf70-48de-4946-8d8f-de06a39d7611', name: "Leonardo Da Vinci", age: StudentAge.threeYears).copyWith(timestamp: DateTime(2025, 10, 4)),
+
+    //* A archivar
+    Student(id: '750c3a46-3e44-4800-bee1-ebccb4f4f55c', name: "Mario Bros Archived", age: StudentAge.threeYears).copyWith(timestamp: DateTime(2025, 1, 10)),
+    Student(id: '9ee64bbd-1011-4849-aec6-eae4a587a8d5', name: "Lopez Garcia Archived", age: StudentAge.threeYears).copyWith(timestamp: DateTime(2025, 1, 11))
+  ];
+
+  static final listOfRegisterBook = <RegisterBook>[
+    RegisterBook(id: 'e0449ae1-ec4d-4eec-a0c2-3b6be2ff46f6', action: "Jose le pego a Pablo", mentions: [listOfStudents[0].toStudentDAO, listOfStudents[1].toStudentDAO], timestamp: DateTime(2025, 1, 5), type: RegisterBookType.incident),
+    RegisterBook(id: 'dd1acb1c-86c2-4ba1-a69f-b1c8e24c01ee', action: "Accion indeterminada 1", timestamp: DateTime(2025, 1, 6)),
+    RegisterBook(id: '06b654e1-2852-4618-84a7-bb2c43a3eba1', action: "Leonardo, Mendo y Bruno han hecho su actividad", timestamp: DateTime(2025, 1, 6), type: RegisterBookType.anecdotal),
+    RegisterBook(id: '10277d6d-630c-4a6e-99ec-f807a64714f6', action: "Al bordo de un barco, Gerardo ha jugado", timestamp: DateTime(2025, 2, 15), mentions: [listOfStudents[4].toStudentDAO], type: RegisterBookType.anecdotal),
+    RegisterBook(id: '3984adaf-70c2-45b1-aecc-e0b56bb43158', action: "Otra accion indeterminada 2", timestamp: DateTime(2025, 1, 1)),
+    RegisterBook(id: 'cd334961-cc2f-4c2a-8477-482155e7ed0e', action: "Desde la vista de Bruno, ha visto como Pablo salía del salon", timestamp: DateTime(2025, 3, 8), mentions: [listOfStudents[3].toStudentDAO, listOfStudents[1].toStudentDAO]),
+
+    //* A archivar
+    RegisterBook(id: '743f923b-3a0e-45a9-a681-85408e5fa521', action: "Accion mal escrota Archived", timestamp: DateTime(2025, 2, 7)),
+    RegisterBook(id: 'c1b271b1-f21f-4c10-9dae-975f627d0c00', action: "Otra accion mal escrota Archived", timestamp: DateTime(2025, 1, 2), mentions: [listOfStudents[5].toStudentDAO], type: RegisterBookType.incident),
+  ];
+
+  static Student studentRecently = listOfStudents.reduce((a, b) => a.timestamp.isAfter(b.timestamp) ? a : b);
+  static Student studentOldy = listOfStudents.reduce((a, b) => a.timestamp.isBefore(b.timestamp) ? a : b);
+  static Student studentHighestName = listOfStudents.reduce((a, b) => a.name.compareTo(b.name) < 0 ? a : b);
+  static Student studentLowestName = listOfStudents.reduce((a, b) => a.name.compareTo(b.name) > 0 ? a : b);
+
+  static RegisterBook registerBookRecently = listOfRegisterBook.reduce((a, b) => a.timestamp.isAfter(b.timestamp) ? a : b);
+  static RegisterBook registerBookOldy = listOfRegisterBook.reduce((a, b) => a.timestamp.isBefore(b.timestamp) ? a : b);
+
+  static Future<void> addingTestData(bool withRegisterBook) async {
+    final studentService = GetIt.I<StudentServiceBase>();
+
+    //* Estudiantes agregados
+    await Future.wait(listOfStudents.map((e) => studentService.addOne(e)));
+
+    //* Estudiantes archivados agregados
+    final studentlistAddedToArchive = await studentService.getAll(StudentFilter(nameLike: "archived"));
+    await Future.wait(studentlistAddedToArchive.getValue!.map((e) => studentService.archiveOne(e.id)));
+
+    if(withRegisterBook) {
+      final registerBookService = GetIt.I<RegisterBookServiceBase>();
+
+      //* Cuadernos de registro agregados
+      await Future.wait(listOfRegisterBook.map((e) => registerBookService.addOne(e)));
+      final registerBookListAddedToArchive = await registerBookService.getAll(RegisterBookFilter(actionLike: "archived"));
+      await Future.wait(registerBookListAddedToArchive.getValue!.map((e) => registerBookService.archiveOne(e.id)));
+    }
+  }
+}
