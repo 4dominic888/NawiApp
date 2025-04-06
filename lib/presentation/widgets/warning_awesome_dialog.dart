d186c3c2616ba1c371fa4fe326494d828e55fa00
4dominic888 <4dominin888@gmail.com>
2025-04-05 21:44:15 -0500

    creando un awesome dialog personalizado

---
+++ lib/presentation/widgets/warning_awesome_dialog.dart
@@ -0,0 +1,29 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class WarningAwesomeDialog extends AwesomeDialog {
+
+  @override
+  DialogType get dialogType => DialogType.warning;
+
+  @override
+  bool get headerAnimationLoop => false;
+
+  @override
+  bool get showCloseIcon => true;
+
+  @override
+  Widget? get closeIcon => const Icon(Icons.close);
+
+  @override
+  AnimType get animType => AnimType.scale;
+
+  WarningAwesomeDialog({
+    required super.context,
+    required super.title,
+    required super.desc,
+    required super.btnOk,
+    required super.btnCancel
+  });
+}
