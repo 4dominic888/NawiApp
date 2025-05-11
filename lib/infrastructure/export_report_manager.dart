fba9a644ef98ad727cbe4751924e69d07e17c5e6
4dominic888 <4dominin888@gmail.com>
2025-05-11 14:40:42 -0500

    refactorizacion y agregado de un generador de pdf por estudiante

--- lib/infrastructure/export_report_manager.dart
+++ lib/infrastructure/export_report_manager.dart
@@ -1,13 +1,47 @@
 import 'dart:io';
 import 'package:file_selector/file_selector.dart';
+import 'package:get_it/get_it.dart';
 import 'package:intl/intl.dart';
 import 'package:nawiapp/domain/classes/filter/register_book_filter.dart';
 import 'package:nawiapp/domain/classes/result.dart';
+import 'package:nawiapp/domain/models/register_book/summary/register_book_summary.dart';
+import 'package:nawiapp/domain/services/register_book_service_base.dart';
+import 'package:nawiapp/infrastructure/fonts/open_sans_font.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
 
 abstract class ExportReportManager {
-  Future<Result<Document>> generate(RegisterBookFilter registerBookFilter);
+
+  List<Widget> buildContent(Context context, Iterable<RegisterBookSummary> data);
+  Widget builHeader(Context context);
+
+  Future<Result<Document>> generate(RegisterBookFilter registerBookFilter) async {
+    final dataExportResult = await GetIt.I<RegisterBookServiceBase>().getAll(registerBookFilter);
+    if(dataExportResult is NawiError) return dataExportResult.getError()!;
+
+    await GetIt.I.isReady<OpenSansFont>();
+    final openSansFont = GetIt.I<OpenSansFont>();
+    final data = dataExportResult.getValue!;
+    final pdf = Document();
+
+    pdf.addPage(
+      MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        theme: ThemeData(defaultTextStyle: TextStyle(
+          fontNormal: openSansFont.regular,
+          fontBold: openSansFont.bold,
+          fontItalic: openSansFont.italic
+        )),
+        header: (context) => builHeader(context),
+        build: (context) => buildContent(context, data),
+      )
+    );
+
+    return Success(data: pdf);
+
+  }
 
   Future<File> saveTemporaly(Document document) async {
     final dir = await getTemporaryDirectory();
@@ -15,6 +49,7 @@ abstract class ExportReportManager {
     await file.writeAsBytes(await document.save());
     return file;
   }
+  
   Future<File?> saveToUserLocation(Document document) async {
     final bytes = await document.save();
     final fileSaveLocation = await getSaveLocation(suggestedName: 'cuaderno_de_registro_reporte_${DateFormat('ddMMyhhmm').format(DateTime.now())}.pdf');
--- lib/infrastructure/register_book_export.dart
+++ lib/infrastructure/register_book_export.dart
@@ -1,76 +1,122 @@
 import 'package:collection/collection.dart';
-import 'package:get_it/get_it.dart';
 import 'package:intl/intl.dart';
-import 'package:nawiapp/domain/classes/filter/register_book_filter.dart';
-import 'package:nawiapp/domain/classes/result.dart';
-import 'package:nawiapp/domain/services/register_book_service_base.dart';
+import 'package:nawiapp/domain/models/register_book/summary/register_book_summary.dart';
 import 'package:nawiapp/infrastructure/export_report_manager.dart';
-import 'package:nawiapp/infrastructure/fonts/open_sans_font.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
 
-class RegisterBookExport extends ExportReportManager {
+
+
+class RegisterBookExportByDate extends ExportReportManager {
+
   @override
-  Future<Result<Document>> generate(RegisterBookFilter registerBookFilter) async {
-    final dataExportResult = await GetIt.I<RegisterBookServiceBase>().getAll(registerBookFilter);
-    if(dataExportResult is NawiError) return dataExportResult.getError()!;
+  Widget builHeader(Context context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('Cuaderno de registro en base a fechas', style: TextStyle().copyWith(fontWeight: FontWeight.bold, fontSize: 17)),
+        Text('Página ${context.pageNumber}')
+      ]
+    );
+  }
 
-    await GetIt.I.isReady<OpenSansFont>();
-    final openSansFont = GetIt.I<OpenSansFont>();
-
-    final data = dataExportResult.getValue!;
-
-    final pdf = Document();
-    pdf.addPage(
-      MultiPage(
-        pageFormat: PdfPageFormat.a4,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        theme: ThemeData(defaultTextStyle: TextStyle(
-          fontNormal: openSansFont.regular,
-          fontBold: openSansFont.bold,
-          fontItalic: openSansFont.italic
-        )),
-        header: (context) => Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Cuaderno de registro en base a fechas', style: TextStyle().copyWith(fontWeight: FontWeight.bold, fontSize: 17)),
-            Text('Página ${context.pageNumber}')
-          ]
-        ),
-        build: (context) {
-          final dataOrderedPerDay = groupBy(
-            data, (e) => DateFormat('EEEE, d MMMM y').format(DateTime(e.createdAt.year, e.createdAt.month, e.createdAt.day))
-          );
-
-          return dataOrderedPerDay.entries.map((entry) => [
-            Header(child: Text(entry.key, style: TextStyle().copyWith(fontWeight: FontWeight.bold))),
-
-            ...entry.value.expand((element) => [
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      color: const PdfColor.fromInt(0xe9e9e9),
-                      child: Bullet(text: element.action),
-                    )
-                  ),
-
-                  SizedBox(width: 8.0),
-
-                  Text(
-                    DateFormat('hh:mm a').format(element.createdAt),
-                    style: const TextStyle().copyWith(fontSize: 12, color: PdfColor.fromInt(0xFF555555))
-                  )
-                ]
-              ),
-
-              SizedBox(height: 4.0)
-            ])
-          ]).expand((e) => e).toList();
-        },
-      )
+  @override
+  List<Widget> buildContent(Context context, Iterable<RegisterBookSummary> data) {
+    final dataOrderedPerDay = groupBy(
+      data, (e) => DateFormat('EEEE, d MMMM y').format(DateTime(e.createdAt.year, e.createdAt.month, e.createdAt.day))
     );
 
-    return Success(data: pdf);
+    return dataOrderedPerDay.entries.map((entry) => [
+      Header(child: Text(entry.key, style: TextStyle().copyWith(fontWeight: FontWeight.bold))),
+
+      ...entry.value.expand((element) => [
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: const PdfColor.fromInt(0xe9e9e9),
+                child: Bullet(text: element.action),
+              )
+            ),
+
+            SizedBox(width: 8.0),
+
+            Text(
+              DateFormat('hh:mm a').format(element.createdAt),
+              style: const TextStyle().copyWith(fontSize: 12, color: PdfColor.fromInt(0xFF555555))
+            )
+          ]
+        ),
+
+        SizedBox(height: 4.0)
+      ])
+    ]).expand((e) => e).toList();
   }
+}
+
+class RegisterBookExportByStudent extends ExportReportManager {
+  @override
+  Widget builHeader(Context context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('Cuaderno de registro ordenado por estudiante', style: TextStyle().copyWith(fontWeight: FontWeight.bold, fontSize: 17)),
+        Text('Página ${context.pageNumber}')
+      ]
+    );
+  }
+
+  @override
+  List<Widget> buildContent(Context context, Iterable<RegisterBookSummary> data) {
+
+    final mapStudentRegisterBook = data.expand((rb) => rb.mentions.map((s) => MapEntry(s, rb)));
+    final dataOrderedByStudent = groupBy(mapStudentRegisterBook, (entry) => entry.key)
+      .map((key, value) => MapEntry(key, value.map((e) => e.value)));
+
+    return dataOrderedByStudent.entries.map((entry) => [
+      Header(child: Text('${entry.key.name} (${entry.key.age.name})', style: TextStyle().copyWith(fontWeight: FontWeight.bold))),
+
+      ...entry.value.expand((element) => [
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: const PdfColor.fromInt(0xe9e9e9),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Bullet(text: element.action, textAlign: TextAlign.start),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            DateFormat('EEEE, d MMMM y').format(element.createdAt),
+                            style: TextStyle().copyWith(fontSize: 8, color: const PdfColor.fromInt(0x2e2e2e)),
+                            textAlign: TextAlign.end
+                          )
+                        ),
+                        SizedBox(width: 8.0)
+                      ]
+                    )
+                  ]
+                ),
+              )
+            ),
+
+            SizedBox(width: 8.0),
+
+            Text(
+              DateFormat('hh:mm a').format(element.createdAt),
+              style: const TextStyle().copyWith(fontSize: 12, color: PdfColor.fromInt(0xFF555555))
+            )
+          ]
+        ),
+
+        SizedBox(height: 4.0)
+      ])
+
+    ]).expand((e) => e).toList();
+  }
+
 }
