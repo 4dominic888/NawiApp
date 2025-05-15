04361a486a442d46be096f589254adffa19d8447
4dominic888 <4dominin888@gmail.com>
2025-05-15 16:00:48 -0500

    agregando DAO y servicio al locator original

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,12 +1,15 @@
 import 'package:get_it/get_it.dart';
 import 'package:nawiapp/data/drift_connection.dart';
+import 'package:nawiapp/domain/daos/classroom_dao.dart';
 import 'package:nawiapp/domain/daos/register_book_dao.dart';
 import 'package:nawiapp/domain/daos/student_register_book_dao.dart';
 import 'package:nawiapp/domain/daos/student_dao.dart';
+import 'package:nawiapp/domain/services/classroom_service_base.dart';
 import 'package:nawiapp/domain/services/register_book_service_base.dart';
 import 'package:nawiapp/domain/services/student_service_base.dart';
 import 'package:nawiapp/infrastructure/fonts/open_sans_font.dart';
 import 'package:nawiapp/infrastructure/in_memory_storage.dart';
+import 'package:nawiapp/presentation/implementations/classroom_service_implement.dart';
 import 'package:nawiapp/presentation/implementations/register_book_service_implement.dart';
 import 'package:nawiapp/presentation/implementations/student_service_implement.dart';
 
@@ -29,6 +32,10 @@ void setupLocator() {
     locator<NawiDatabase>()
   ));
 
+  locator.registerLazySingleton<ClassroomDAO>(() => ClassroomDAO(
+    locator<NawiDatabase>()
+  ));
+
   //* Services
   locator.registerLazySingleton<StudentServiceBase>(() => StudentServiceImplement(
     locator<StudentDAO>(), locator<StudentRegisterBookDAO>()
@@ -36,6 +43,10 @@ void setupLocator() {
 
   locator.registerLazySingleton<RegisterBookServiceBase>(() => RegisterBookServiceImplement(
     locator<RegisterBookDAO>(), locator<StudentRegisterBookDAO>()
+  ));
+
+  locator.registerLazySingleton<ClassroomServiceBase>(() => ClassroomServiceImplement(
+    locator<ClassroomDAO>()
   ));
 
   //* Extra
