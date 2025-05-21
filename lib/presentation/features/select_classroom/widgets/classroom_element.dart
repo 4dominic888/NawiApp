ad56fbb1641476ca3223965acb1079ab98b88ee2
4dominic888 <4dominin888@gmail.com>
2025-05-21 15:10:10 -0500

    UI no final xd

--- lib/presentation/features/select_classroom/widgets/classroom_element.dart
+++ lib/presentation/features/select_classroom/widgets/classroom_element.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 import 'package:nawiapp/domain/models/classroom/entity/classroom.dart';
+import 'package:nawiapp/domain/services/classroom_service_base.dart';
+import 'package:nawiapp/presentation/features/select_classroom/providers/select_classroom_grid_provider.dart';
 import 'package:nawiapp/presentation/features/select_classroom/screens/add_classroom_modal.dart';
+import 'package:nawiapp/presentation/widgets/loading_process_button.dart';
+import 'package:nawiapp/presentation/widgets/warning_awesome_dialog.dart';
+import 'package:rounded_loading_button_plus/rounded_loading_button.dart';
 
 class ClassroomElement extends StatelessWidget {
 
@@ -17,44 +24,90 @@ class ClassroomElement extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
               spacing: 10,
+              crossAxisAlignment: WrapCrossAlignment.center,
               children: [
                 Icon(IconData(item.iconCode, fontFamily: 'MaterialIcons')),
-                Text(item.name, overflow: TextOverflow.ellipsis),
-                IconButton(
-                  onPressed: () {
-                    
-                  },
-                  icon: const Icon(Icons.play_circle_fill_outlined)
+                SizedBox(
+                  width: 200,
+                  child: Text(item.name, overflow: TextOverflow.ellipsis)
                 )
-              ],
+              ]
             ),
 
-            PopupMenuButton(
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  child: const Text('Editar'),
-                  onTap: () => Navigator.of(context).push(
-                    DialogRoute(context: context, builder: (_) => AddClassroomModal(data: item))
+            //TODO Estadisticas info
+            Placeholder(),
+
+            Divider(),
+
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Visitar aula')
                   ),
                 ),
-
-                PopupMenuItem(
-                  child: const Text('Eliminar'),
-                  onTap: () {
-                    
-                  },
-                )
+                PopupClassroomOptions(item: item),
               ],
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class PopupClassroomOptions extends ConsumerWidget {
+  const PopupClassroomOptions({
+    super.key,
+    required this.item,
+  });
+
+  final Classroom item;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return PopupMenuButton(
+      itemBuilder: (context) => [
+        PopupMenuItem(
+          child: const Text('Editar'),
+          onTap: () => Navigator.of(context).push(
+            DialogRoute(context: context, builder: (_) => AddClassroomModal(data: item))
+          ),
+        ),
+    
+        PopupMenuItem(
+          child: const Text('Eliminar'),
+          onTap: () async {
+            final deleteBtnController = RoundedLoadingButtonController();
+            await WarningAwesomeDialog(
+              title: "Confirmación de eliminación",
+              desc: '¿Estás seguro/a que deseas eliminar esta aula? \n(Esta acción eliminará todos los estudiantes y registros dentro)',
+              context: context,
+              btnOk: LoadingProcessButton(
+                controller: deleteBtnController,
+                autoResetable: true,
+                proccess: () async {
+                  final result = await GetIt.I<ClassroomServiceBase>().deleteOne(item.id);
+                  result.onValue(
+                    onSuccessfully: (__, _) => deleteBtnController.success(),
+                    onError: (__, _) => deleteBtnController.error(),
+                  );
+                  ref.read(classroomSearchProvider.notifier).refresh();
+                  if(context.mounted) Navigator.of(context).pop();
+                },
+                color: Colors.redAccent.shade200,
+                label: const Text("Eliminar", style: TextStyle(color: Colors.white))
+              ),
+            ).show();
+          },
+        )
+      ],
     );
   }
 }
