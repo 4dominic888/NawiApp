521fbfd69f001d72a8dee3b0014dd2ae0fbd349f
4dominic888 <4dominin888@gmail.com>
2025-08-30 20:44:35 -0500

    Tests para generar reportes segun fecha y estudiante

--- test/integration_test/generate_report_test.dart
+++ test/integration_test/generate_report_test.dart
@@ -12,7 +12,7 @@ import '../nawi_test_utils.dart' as testil;
 void main() {
   setUp(() async => await testil.setupIntegrationTestLocator(withRegisterBook: true));
   tearDown(testil.onTearDownSetupIntegrationTestLocator);  
-  test("Correcta estructura de pdf", () async {
+  test("Correcta estructura de pdf en fechas", () async {
     final query = RegisterBookFilter(orderBy: RegisterBookViewOrderByType.timestampOldy, classroomId: '6615024f-0153-4492-b06e-0cb108f90ac6');
     final ExportReportManager exporter = RegisterBookExportByDate();
     
@@ -26,7 +26,34 @@ void main() {
     final outputDir = Directory('test/pdf_test_output');
     if(!await outputDir.exists()) await outputDir.create(recursive: true);
 
-    final outputFile = File('${outputDir.path}/report.pdf');
+    final outputFile = File('${outputDir.path}/date_report.pdf');
+    await outputFile.writeAsBytes(await doc.save());
+
+    testil.customExpect(await outputFile.exists(), isTrue,
+      about: "PDF generado", n: 2
+    );
+
+    testil.customExpect(await outputFile.length(), greaterThan(0),
+      about: "PDF con datos", n: 2
+    );
+    
+  });
+
+  test("Correcta estructura de pdf por estudiantes", () async {
+    final query = RegisterBookFilter(orderBy: RegisterBookViewOrderByType.timestampOldy, classroomId: '6615024f-0153-4492-b06e-0cb108f90ac6');
+    final ExportReportManager exporter = RegisterBookExportByStudent();
+    
+    final result = await exporter.generate(query);
+    testil.customExpect(result, isA<Success>(),
+      about: "Query valida", n: 1
+    );
+
+    final doc = result.getValue!;
+
+    final outputDir = Directory('test/pdf_test_output');
+    if(!await outputDir.exists()) await outputDir.create(recursive: true);
+
+    final outputFile = File('${outputDir.path}/student_report.pdf');
     await outputFile.writeAsBytes(await doc.save());
 
     testil.customExpect(await outputFile.exists(), isTrue,
